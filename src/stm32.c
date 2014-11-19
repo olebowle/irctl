olebowle/irctl

commit 7a81c0575b6ff547b40a2c03eca3adf0db0ba8de

make arguments more friendly for non-programmers

src/stm32.c
@@ -155,9 +155,9 @@ stm32_prepare_buf(struct rc_device *dev, uint8_t * const buf, size_t n)
 	/* process sub argument if available*/
 	if (args.sub_arg) {
 		arg = atoi(args.sub_arg);
-		if (arg < 0 ||
-		    (args.cmd == CMD_MACRO && arg > dev->macro_slots[0] - 1) ||
-		    (args.cmd == CMD_WAKE && arg > dev->wake_slots[0] - 1)) {
+		if (arg < 1 ||
+		    (args.cmd == CMD_MACRO && arg > dev->macro_slots[0]) ||
+		    (args.cmd == CMD_WAKE && arg > dev->wake_slots[0])) {
 			fprintf(stderr, "sub argument out of range\n");
 			return -1;
 		}
@@ -167,7 +167,7 @@ stm32_prepare_buf(struct rc_device *dev, uint8_t * const buf, size_t n)
 	/* process if available, this should only be possible with CMD_MACRO */
 	if (args.ir) {
 		arg = atoi(args.ir);
-		if (arg < 0 || arg > dev->macro_depth[0] - 1) {
+		if (arg < 0 || arg > dev->macro_depth[0]) {
 			fprintf(stderr, "ir argument out of range\n");
 			return -1;
 		}

src/irctl.c
@@ -35,12 +35,12 @@ usage (char *name)
 		"	1. Get wakeup time in seconds from now\n"
 		"		%s -dstm32 -a -g /dev/hidraw0\n"
 		"	2. Set first wakeup ir-code to 0x112233445566\n"
-		"		%s -dstm32 -w0 -s0x112233445566 /dev/hidraw0\n"
+		"		%s -dstm32 -w1 -s0x112233445566 /dev/hidraw0\n"
 		"	3. Set third macro to be triggered by 0x112233445566,\n"
 		"	   which will when send out two commands (1: 0x778899AABBCC, 2: 0xDDEEFF001122)\n"
-		"		%s -dstm32 -m2 -i0 -s0x112233445566 /dev/hidraw0\n"
-		"		%s -dstm32 -m2 -i1 -s0x778899AABBCC /dev/hidraw0\n"
-		"		%s -dstm32 -m2 -i2 -s0xDDEEFF001122 /dev/hidraw0\n", name, name, name, name, name);
+		"		%s -dstm32 -m3 -i0 -s0x112233445566 /dev/hidraw0\n"
+		"		%s -dstm32 -m3 -i1 -s0x778899AABBCC /dev/hidraw0\n"
+		"		%s -dstm32 -m3 -i2 -s0xDDEEFF001122 /dev/hidraw0\n", name, name, name, name, name);
 	exit(EXIT_FAILURE);
 }
 

commit b9b3e63bca11ae2939f8c0be501510a3c5d4e913

fix bug introduced in 7a81c05

src/stm32.c
@@ -165,7 +165,7 @@ stm32_prepare_buf(struct rc_device *dev, uint8_t * const buf, size_t n)
 			fprintf(stderr, "sub argument out of range\n");
 			return -1;
 		}
-		buf[idx++] = arg;
+		buf[idx++] = arg - 1;
 	}
 
 	/* process if available, this should only be possible with CMD_MACRO */

testEEPROM.sh
@@ -12,7 +12,7 @@ done
 for w_slot in $(seq $wake_slots); do
 	./irctl -dstm32 -w${w_slot} -g /dev/hidraw0
 done
-
+#exit 0
 echo "write pattern to eeprom"
 i=0
 for m_slot in $(seq $macro_slots); do